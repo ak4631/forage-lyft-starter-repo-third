ea5fbe1333d7ca8d22da024533f15777aa24c0c1
ak4631 <132299371+ak4631@users.noreply.github.com>
2023-06-10 16:33:17 +0530

    some-changes

---
+++ CarFactory/car_factory.py
@@ -0,0 +1,23 @@
+from car import Car
+from engine.model.capulet_engine import CapuletEngine
+from engine.model.sternman_engine import SternmanEngine
+from engine.model.willoughby_engine import WilloughbyEngine
+
+
+class CarFactory:
+    @staticmethod
+
+    def create_calliope(current_date,last_service_date,current_mileage,last_service_mileage):
+        pass
+
+    def create_glissade(current_date,last_service_date,current_mileage,last_service_mileage):
+        pass
+
+    def create_palindrome(current_date,last_service_date,warning_light_on):
+        pass
+
+    def create_roschach(current_date,last_service_date,current_mileage,last_service_mileage):
+        pass
+    
+    def create_thovex(current_date,last_service_date,current_mileage,last_service_mileage):
+        pass
